00aeb91fad7dbcf784ecba19fe790754db6d413f
Mike Magruder <mikemag@outlook.com>
2020-03-24 12:26:29 -0700

    Update media_dir
    Point the local media_dir to my Downloads directory, since I don't back that up and it's perfect for the temp output from manim.

--- manimlib/constants.py
+++ manimlib/constants.py
@@ -23,7 +23,7 @@ def initialize_directories(config):
         else:
             MEDIA_DIR = os.path.join(
                 os.path.expanduser('~'),
-                "Dropbox (3Blue1Brown)/3Blue1Brown Team Folder"
+                "Downloads"
             )
         if not os.path.isdir(MEDIA_DIR):
             MEDIA_DIR = "./media"
